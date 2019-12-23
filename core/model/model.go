3f338361175cf188d6cf5b533afba035aefd863c
TGXW <XAIWEIER@outlook.com>
2019-12-23 17:47:55 +0800

    add model

--- core/model/model.go
+++ core/model/model.go
@@ -9,7 +9,7 @@ import (
 // User 用户表
 type User struct {
 	// gorm.Model
-	ID         int64  `gorm:"primary_key"`
+	ID         int64  `gorm:"primary_key;auto_increment"`
 	Name       string `gorm:"type:varchar(100);not null;unique"`
 	Pwd        string `gorm:"not null"`
 	Nick       string `gorm:"not null"`
@@ -22,9 +22,62 @@ func (u User) TableName() string {
 	return "t_user"
 }
 
+type Role struct {
+	ID   int64  `gorm:"primary_key;auto_increment"`
+	Name string `gorm:"type:varchar(100);not null;unique"`
+}
+
+func (r Role) TableName() string {
+	return "t_role"
+}
+
+type UserRole struct {
+	ID  int64 `gorm:"primary_key;auto_increment"`
+	Uid User  `gorm:"ForeignKey:ID`
+	Rid Role  `gorm:"ForeignKey:ID`
+}
+
+func (u UserRole) TableName() string {
+	return "t_user_role"
+}
+
+type Browser struct {
+	ID   int64  `gorm:"primary_key;auto_increment"`
+	Name string `gorm:"type:varchar(100);not null;unique"`
+	Uid  User   `gorm:"ForeignKey:ID`
+	Desc string `gorm:"type:varchar(255)"`
+}
+
+func (b Browser) TableName() string {
+	return "t_browser"
+}
+
+type BrowserDeploy struct {
+	ID   int64   `gorm:"primary_key;auto_increment"`
+	Name string  `gorm:"type:varchar(100);not null;unique"`
+	Uid  User    `gorm:"ForeignKey:ID`
+	Desc string  `gorm:"type:varchar(255)"`
+	Bid  Browser `gorm:"ForeignKey:ID`
+}
+
+func (b BrowserDeploy) TableName() string {
+	return "t_browser_deploy"
+}
+
 func init() {
-	table := db.DB.HasTable(User{})
-	if !table {
-		db.DB.CreateTable(User{})
+	if table := db.DB.HasTable(User{}); !table {
+		db.DB.CreateTable(&User{})
+	}
+	if table := db.DB.HasTable(Role{}); !table {
+		db.DB.CreateTable(&Role{})
+	}
+	if table := db.DB.HasTable(UserRole{}); !table {
+		db.DB.CreateTable(&UserRole{})
+	}
+	if table := db.DB.HasTable(Browser{}); !table {
+		db.DB.CreateTable(&Browser{})
+	}
+	if table := db.DB.HasTable(BrowserDeploy{}); !table {
+		db.DB.CreateTable(&BrowserDeploy{})
 	}
 }
